530ba1f68bc8b2b1404d017a93c908d8fca33486
Stefan Haeling <stefanhaeling95@gmail.com>
2020-11-03 23:51:51 +0100

    Added stuff for things

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const robots = require('express-robots-txt');
 const PORT = process.env.PORT || 80;
 
 var sass = require('sass');
@@ -8,10 +9,16 @@ sass.render({file: 'public/stylesheets/_index.scss'}, function (err, result) {})
 express()
     .use(express.static(path.join(__dirname, 'public')))
 
+    .use(robots(__dirname + '/robots.txt'))
+    .use(robots({ UserAgent: '*', Disallow: '/' }))
+    .use(robots({ UserAgent: '*', Disallow: '/', Sitemap: 'http://www.kellario.de/sitemap.xml' }))
+
     .get('/',(req, res) => res.render('pages/index'))
     .get('/snake',(req, res) => res.render('pages/snake/main'))
     .get('/commands', (req, res) => res.render('pages/commands/main'))
     .get('/orbiter', (req, res) => res.render('pages/orbiter/main'))
+    .get('/robots.txt', (req, res) => res.renderFile('robots.txt'))
+    .get('/sitemap.xml', (req, res) => res.render('sitemap.xml'))
 
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
