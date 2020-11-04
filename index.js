7f08402eab93ffc8cecfb54bf7ab6aa1f7d1618b
Stefan Haeling <stefanhaeling95@gmail.com>
2020-11-04 21:42:27 +0100

    Added SSL

--- index.js
+++ index.js
@@ -2,25 +2,51 @@ const express = require('express');
 const path = require('path');
 const robots = require('express-robots-txt');
 const PORT = process.env.PORT || 80;
+const PORTDEFAULT = process.env.PORT || 8000;
+const PORTSSL = process.env.PORT || 443;
 
 var sass = require('sass');
 sass.render({file: 'public/stylesheets/_index.scss'}, function (err, result) {})
 
-express()
+var http = express();
+
+http.get('*', function(req, res) {
+    res.redirect('https://www.kellario.de' + req.url);
+})
+
+http.listen(PORT, () => console.log(`Redirecting from ${ PORT }`));
+http.listen(PORTDEFAULT, () => console.log(`Redirecting from ${ PORTDEFAULT }`));
+
+const https = require("https"),
+    fs = require("fs");
+
+var key = fs.readFileSync("/root/user/.ssl/kellario.key"),
+    cert = fs.readFileSync("/root/user/.ssl/primary.crt"),
+    ca = fs.readFileSync("/root/user/.ssl/intermediate.crt");
+
+const options = {
+    key: key,
+    cert: cert,
+    ca: ca
+};
+
+const app = express();
+
+app
     .use(express.static(path.join(__dirname, 'public')))
 
     .use(robots(__dirname + '/robots.txt'))
-    .use(robots({ UserAgent: '*', Disallow: '/' }))
-    .use(robots({ UserAgent: '*', Disallow: '/', Sitemap: 'http://www.kellario.de/sitemap.xml' }))
+    .use(robots({ UserAgent: '*', Disallow: '/no-bots', Sitemap: 'http://www.kellario.de/sitemap.xml' }))
 
     .get('/',(req, res) => res.render('pages/index'))
     .get('/snake',(req, res) => res.render('pages/snake/main'))
     .get('/commands', (req, res) => res.render('pages/commands/main'))
     .get('/orbiter', (req, res) => res.render('pages/orbiter/main'))
     .get('/robots.txt', (req, res) => res.renderFile('robots.txt'))
-    .get('/sitemap.xml', (req, res) => res.render('sitemap.xml'))
+    .get('/sitemap.xml', (req, res) => res.renderFile('sitemap.xml'))
 
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
+;
 
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+https.createServer(options, app).listen(PORTSSL, console.log(`Listening on ${ PORTSSL }`));
